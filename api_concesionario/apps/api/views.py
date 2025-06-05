82233af50e7e537977ccd981c7f0358015e7aa7b
juan perez <jplesmes19@gmail.com>
2025-06-04 21:44:52 -0500

    Se realiza la logica de la API para ser consumida por la interfaz

--- api_concesionario/apps/api/views.py
+++ api_concesionario/apps/api/views.py
@@ -18,13 +18,39 @@ class ApiConcesionario(APIView):
     
     # Generar el endpoint para crear un nuevo objeto
     def post(self, request):
-        pass
+        try:        
+            # Validar que los datos no lleguen vacios
+            if request.data['marca'] != "" and request.data['surcursal'] != "" and request.data['aspirante'] != "":
+                concesionario = ConcesionarioSerializer(data=request.data) # Crear el dato que llega de VUE
+                if concesionario.is_valid(): # Validar que el dato sea valido
+                    concesionario.save() # Guardar el objeto
+                return Response({'data':True}, status=st.HTTP_201_CREATED)
+            return Response({'data':False}, status=st.HTTP_400_BAD_REQUEST) # Devolver una mala respuesta
+        except Exception as e:
+            pass
+        return Response({'data':False}, status=st.HTTP_400_BAD_REQUEST) # Devolver una mala respuesta
 
     # Generar el endpoint para actualizar un objeto
     def put(self, request, pk):
-        pass
+        try:
+            concesionario = Concesionario.objects.get(pk=pk) # Obtener el objeto
+            # Validar que los datos no lleguen vacios
+            if request.data['marca'] != "" and request.data['surcursal'] != "" and request.data['aspirante'] != "":
+                concesionario.marca = request.data['marca'] # Actualizar el objeto
+                concesionario.surcursal = request.data['surcursal'] # Actualizar el objeto
+                concesionario.aspirante = request.data['aspirante'] # Actualizar el objeto
+                concesionario.save() # Guardar el objeto
+                return Response({'update':True}, status=st.HTTP_200_OK)
+        except Exception as e:
+            return Response({'update':False}, status=st.HTTP_400_BAD_REQUEST) # Devolver una mala respuesta
+        
 
 
     # Generar el endpoint para eliminar un objeto
     def delete(self, request, pk):
-        pass
+        try:
+            Concesionario.objects.get(pk=pk).delete() # Eliminar el objeto
+            return Response({'delete':True}, status=st.HTTP_200_OK)
+        except Exception as e:
+            return Response({'delete':False}, status=st.HTTP_400_BAD_REQUEST) # Devolver una mala respuesta
+        
