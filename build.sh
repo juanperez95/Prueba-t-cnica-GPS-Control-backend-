b25d5f9de75f80ae455d31f9f87bbc8fe7c7a427
juan perez <jplesmes19@gmail.com>
2025-06-05 15:31:43 -0500

    Corregir el build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -o errexit
 pip install -r requirements.txt
 
 # Convert static asset files
-python manage.py collectstatic --no-input
+python api_concesionario/manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python manage.py migrate
+python api_concesionario/manage.py migrate
